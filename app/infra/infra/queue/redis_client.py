af4a3f330d1d7371358c14eabd2ac4aecbd50755
ayabaghdady <ayabaghdadi89@gmail.com>
2026-05-13 11:49:54 +0300

    Add infra and ingest worker setup

---
+++ app/infra/infra/queue/redis_client.py
@@ -0,0 +1,10 @@
+from redis import Redis
+
+from app.core.config import settings
+
+
+redis_conn = Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    decode_responses=True,
+)
